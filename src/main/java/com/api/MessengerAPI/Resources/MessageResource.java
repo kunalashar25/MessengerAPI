203e47639484efe77f724ff616f60fc708caea24
kunalashar.25@gmail.com <kunal0310>
2018-03-01 01:05:03 +0530

    code updated.

--- src/main/java/com/api/MessengerAPI/Resources/MessageResource.java
+++ src/main/java/com/api/MessengerAPI/Resources/MessageResource.java
@@ -3,8 +3,10 @@ package com.api.MessengerAPI.Resources;
 import java.util.List;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -14,28 +16,40 @@ import com.api.MessengerAPI.Model.Message;
 import com.api.MessengerAPI.Service.MessageService;
 
 @Path("/messages")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class MessageResource
 {
 	MessageService messageService = new MessageService();
 
 	@GET
-	@Produces(MediaType.APPLICATION_JSON)
 	public List<Message> getMessages()
 	{
 		return messageService.getAllMessages();
-
 	}
 
 	@POST
-	@Consumes(MediaType.APPLICATION_JSON)
-	@Produces(MediaType.APPLICATION_JSON)
 	public Message addMessage(Message message)
 	{
 		return messageService.addMessage(message);
 	}
 
+	@PUT
+	@Path("/{messageId}")
+	public Message updateMessage(@PathParam("messageId") long messageId, Message message)
+	{
+		message.setId(messageId);
+		return messageService.updateMessage(message);
+	}
+
+	@DELETE
+	@Path("/{messageId}")
+	public void deleteMessage(@PathParam("messageId") long messageId)
+	{
+		messageService.removeMessage(messageId);
+	}
+
 	@GET
-	@Produces(MediaType.APPLICATION_JSON)
 	@Path("/{messageId}")
 	public Message getMessage(@PathParam("messageId") long messageId)
 	{
