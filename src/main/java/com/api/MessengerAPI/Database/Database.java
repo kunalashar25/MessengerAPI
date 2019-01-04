7bace8a66d3ef41496d086633358437bd75119cb
asku8001 <asku8001@INLJ4HP4M2.enterprisenet.org>
2019-01-04 11:14:41 +0530

    code added for profile section

--- src/main/java/com/api/MessengerAPI/Database/Database.java
+++ src/main/java/com/api/MessengerAPI/Database/Database.java
@@ -9,14 +9,14 @@ import com.api.MessengerAPI.Model.Profile;
 public class Database
 {
 	private static Map<Long, Message>	messages	= new HashMap<Long,Message>();
-	private static Map<Long, Profile>	profiles	= new HashMap<Long,Profile>();
+	private static Map<String, Profile>	profiles	= new HashMap<String,Profile>();
 
 	public static Map<Long, Message> getMessages()
 	{
 		return messages;
 	}
 
-	public static Map<Long, Profile> getProfiles()
+	public static Map<String, Profile> getProfiles()
 	{
 		return profiles;
 	}
